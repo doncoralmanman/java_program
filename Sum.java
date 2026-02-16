3742b844b3a6ba85ac16e7b4f6e9937567ca2faa
AARNAV <doncoralmanman@gmail.com>
2026-02-16 10:06:41 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Sum
+  {
+    public static void main(String args[])
+    {
+      Scanner S=new Scanner(System.in);
+      int a=S.nextInt();
+      int b=S.nextInt();
+      System.out.println("sum:"+(a+b));
+    }
+  }
